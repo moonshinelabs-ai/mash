1c746b8adf85b783ba1ead910a429d0a23e0ebf1
Nate Harada <nharada1@gmail.com>
2023-10-23 13:07:50 -0700

    Fix and lint

--- mash/mash/images/__init__.py
+++ mash/mash/images/__init__.py
@@ -1,5 +1,5 @@
 from .conversion import to_numpy, to_pil, to_tensor
 from .crop import (center_square_crop, crop_rectangle, crop_square,
-                   random_square_crop, crop_to_multiple_of_dimension)
-from .truecolor import grayscale_to_rgb, transparent_to_rgb
+                   crop_to_multiple_of_dimension, random_square_crop)
 from .resize import resize_image_max_side, resize_image_min_side
+from .truecolor import grayscale_to_rgb, transparent_to_rgb
